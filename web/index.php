2cfd065a0027e29928abb14356e17580b5434110
Joseph Morrill <joseph.evan.morrill@gmail.com>
2016-03-01 09:34:50 -0500

    Styled sticky notes to more closely resemble prototype

--- web/index.php
+++ web/index.php
@@ -85,6 +85,9 @@
 	<style>
 	/* Styling from http://cssdeck.com/labs/css3-sticky-note */
 	@import url(http://fonts.googleapis.com/css?family=Gloria+Hallelujah);
+	body{
+		background:url(cork.jpg);
+	}
 	.note-wrapper{
 		display:inline-block;
 		margin:20px;
@@ -97,19 +100,24 @@
 		outline:0px;
 		font-size:18px;
 		font-family:'Gloria Hallelujah', cursive, Lucida Handwriting, Lucida, Cambria, serif;
-		padding:25px 25px 40px;
+		padding:30px 20px 15px 20px;
 		min-height:200px;
 		min-width:200px;
 		width:210px;
 		line-height:1.5;
 		border:0;
 		border-radius:3px;
-		background: -webkit-linear-gradient(#F9EFAF, #F7E98D);
-		background: -moz-linear-gradient(#F9EFAF, #F7E98D);
-		background: -o-linear-gradient(#F9EFAF, #F7E98D);
-		background: -ms-linear-gradient(#F9EFAF, #F7E98D);
-		background: linear-gradient(#F9EFAF, #F7E98D);
+		background:-webkit-linear-gradient(#F9EFAF, #F7E98D);
+		background:-moz-linear-gradient(#F9EFAF, #F7E98D);
+		background:-o-linear-gradient(#F9EFAF, #F7E98D);
+		background:-ms-linear-gradient(#F9EFAF, #F7E98D);
+		background-repeat:no-repeat;
+		background-position:center;
+		background:url( pin.png ), linear-gradient(#F9EFAF, #F7E98D);
+		background-repeat:no-repeat, no-repeat;
+		background-position:center -1px, center;
 		box-shadow:0 4px 6px rgba(0,0,0,0.1);
+		box-shadow:1px 1px 5px black;
 		overflow:hidden;
 		resize:both;
 		z-index:1;
@@ -127,6 +135,10 @@
 	}
 	.note-wrapper:hover .note-delete{
 		display:block;
+	}
+	button{
+		font-size:20px;
+		padding:4px 6px;
 	}
 	</style>
 </head>
