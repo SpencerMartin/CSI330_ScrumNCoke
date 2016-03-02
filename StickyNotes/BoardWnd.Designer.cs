45e5bd0f02772c123b9e5edcdabe74389c23ebe9
Joseph Morrill <joseph.evan.morrill@gmail.com>
2016-03-02 16:28:16 -0500

    Joe's C# test app

---
+++ StickyNotes/BoardWnd.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace StickyNotes {
+	partial class BoardWnd {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing ) {
+			if( disposing && ( components != null ) ) {
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.label1 = new System.Windows.Forms.Label();
+			this.scrollV = new System.Windows.Forms.VScrollBar();
+			this.scrollH = new System.Windows.Forms.HScrollBar();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Image = global::StickyNotes.Properties.Resources.note;
+			this.label1.Location = new System.Drawing.Point(931, 39);
+			this.label1.Name = "label1";
+			this.label1.Padding = new System.Windows.Forms.Padding(20, 40, 20, 40);
+			this.label1.Size = new System.Drawing.Size(250, 250);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "This is a test sticky note";
+			this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.StickyMouseDown);
+			this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.StickyMouseMove);
+			this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.StickyMouseUp);
+			// 
+			// scrollV
+			// 
+			this.scrollV.Location = new System.Drawing.Point(1220, -2);
+			this.scrollV.Name = "scrollV";
+			this.scrollV.Size = new System.Drawing.Size(17, 653);
+			this.scrollV.TabIndex = 1;
+			// 
+			// scrollH
+			// 
+			this.scrollH.Location = new System.Drawing.Point(0, 634);
+			this.scrollH.Name = "scrollH";
+			this.scrollH.Size = new System.Drawing.Size(1220, 17);
+			this.scrollH.TabIndex = 2;
+			// 
+			// BoardWnd
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::StickyNotes.Properties.Resources.cork;
+			this.ClientSize = new System.Drawing.Size(1237, 651);
+			this.Controls.Add(this.scrollH);
+			this.Controls.Add(this.scrollV);
+			this.Controls.Add(this.label1);
+			this.Name = "BoardWnd";
+			this.Text = "Scrum Board";
+			this.Load += new System.EventHandler(this.BoardWnd_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.VScrollBar scrollV;
+		private System.Windows.Forms.HScrollBar scrollH;
+	}
+}
+
---
+++ StickyNotes/BoardWnd.cs
@@ -0,0 +1,80 @@
+﻿/* Referenced:
+ * http://stackoverflow.com/questions/556147/how-to-quickly-and-easily-embed-fonts-in-winforms-app-in-c-sharp
+ * http://stackoverflow.com/questions/16384903/moving-a-control-by-dragging-it-with-the-mouse-in-c-sharp
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+
+namespace StickyNotes {
+	public partial class BoardWnd : Form {
+		[System.Runtime.InteropServices.DllImport( "gdi32.dll" )]
+		private static extern IntPtr AddFontMemResourceEx( IntPtr pbFont, uint cbFont,
+			IntPtr pdv, [System.Runtime.InteropServices.In] ref uint pcFonts );
+
+		private PrivateFontCollection fonts = new PrivateFontCollection();
+
+		Font Gloria;
+
+		public BoardWnd() {
+			InitializeComponent();
+
+			// Load Gloria Hallelujah font
+			byte[] fontData = Properties.Resources.GloriaHallelujah;
+			IntPtr fontPtr = System.Runtime.InteropServices.Marshal.AllocCoTaskMem( fontData.Length );
+			System.Runtime.InteropServices.Marshal.Copy( fontData, 0, fontPtr, fontData.Length );
+			uint dummy = 0;
+			fonts.AddMemoryFont( fontPtr, Properties.Resources.GloriaHallelujah.Length );
+			AddFontMemResourceEx( fontPtr, (uint)Properties.Resources.GloriaHallelujah.Length, IntPtr.Zero, ref dummy );
+			System.Runtime.InteropServices.Marshal.FreeCoTaskMem( fontPtr );
+
+			Gloria = new Font( fonts.Families[0], 16.0F );
+
+			// Test label2
+			Label label2 = new System.Windows.Forms.Label();
+			label2.BackColor = System.Drawing.Color.Transparent;
+			label2.Font = Gloria;
+			label2.Image = global::StickyNotes.Properties.Resources.note;
+			label2.Location = new System.Drawing.Point( 137, 236 );
+			label2.Name = "label2";
+			label2.Padding = new System.Windows.Forms.Padding( 20, 40, 20, 40 );
+			label2.Size = new System.Drawing.Size( 250, 250 );
+			label2.TabIndex = 0;
+			label2.Text = "This is a test sticky note 2";
+			this.Controls.Add( label2 );
+		}
+
+		private void BoardWnd_Load( object sender, EventArgs e ) {
+			label1.Font = Gloria;
+		}
+
+		private Point _Offset = Point.Empty;
+
+		protected void StickyMouseDown( object sender, MouseEventArgs e ) {
+			if( e.Button == MouseButtons.Left ) {
+				_Offset = new Point( e.X, e.Y );
+			}
+		}
+
+		protected void StickyMouseMove( object sender, MouseEventArgs e ) {
+			if( _Offset != Point.Empty ) {
+				Label thisSticky = (Label) sender;
+				Point newlocation = thisSticky.Location;
+				newlocation.X += e.X - _Offset.X;
+				newlocation.Y += e.Y - _Offset.Y;
+				thisSticky.Location = newlocation;
+			}
+		}
+
+		protected void StickyMouseUp( object sender, MouseEventArgs e ) {
+			_Offset = Point.Empty;
+		}
+	}
+}
