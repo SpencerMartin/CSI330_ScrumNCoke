1176d5e38171ca44f25cea8ca435ce7c07f83afa
Joseph Morrill <joseph.evan.morrill@gmail.com>
2016-03-02 19:51:52 -0500

    Converted from JavaScript to jQuery

--- web/index.php
+++ web/index.php
@@ -2,81 +2,120 @@
 <html>
 <head>
 	<title>Scrum Notes</title>
-	<script src='http://projects.josephmorrill.com/library/versions/latest.js'></script>
 	<meta name='viewport' content='initial-scale=1'>
+	<script src='https://code.jquery.com/jquery-2.2.1.min.js'></script>
+	<script src='https://code.jquery.com/ui/1.11.4/jquery-ui.min.js'></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<script>
 	var notes = {
 		"create" : function(){
-			getJSON( 'api.php?action=create', true, function( data ){
-				notes.show( data.id, "" );
-			} );
-		},
-		"update" : function( note ){
-			var note_id = $.get( note, 'id' );
-			var note_content = note.getElementsByClassName( 'note' )[0];
-			$.set( note, 'status', 'updating' );
-			getText( "api.php?action=update&id=" + note_id + "&content=" + encodeURIComponent( note_content.value ), false, function(){
-				// Request finished, so change note status to not updating
-				$.set( note, 'status', 'updated' );
-			} );
-		},
-		"delete" : function( note ){
+			// Sends a GET request to api.php?action=create and get ID back, creating a new note
+			$.get( 'api.php', {
+				'action' : 'create'
+			} ).done( function whenDone( data_text ){
+				try{
+					// Convert text received from request into JavaScript object
+					var data = JSON.parse( data_text );
 
-			var note_id = $.get( note, 'id' );
-			getJSON( 'api.php?action=delete&id=' + note_id, true, function( data ){
-				$.delete( note );
+					// Create note element with ID returned from database, and empty note text
+					notes.show( data.id, "" );
+				}catch( e ){
+					console.log( 'Could not parse data for notes.create', data_text );
+					alert( 'Error parsing data. See console for details.' );
+				}
+			} );
+		},
+		"update" : function( note_element ){
+			// Sends a GET request to api.php?action=update to change database record for note
+
+			// Get note data
+			var note = $( note_element );
+			var note_id = note.data( 'id' );
+			var note_content = $( note.children( '.note' )[0] );
+
+			// Mark the note as updating
+			note.data( 'status', 'updating' );
+
+			// Send update to server, and mark as "updated" when done
+			$.get( 'api.php', {
+				'action' : 'update',
+				'id' : note_id,
+				'content' : note_content.val()
+			} ).done( function whenDone( data_text ){
+				note.data( 'status', 'updated' );
+			} );
+		},
+		"delete" : function( note_element ){
+			// Sends a GET request to api.php?action=delete to change database record for note
+
+			// Get note data
+			var note = $( note_element );
+			var note_id = note.data( 'id' );
+
+			// Send delete request to server, and remove note from screen when done
+			$.get( 'api.php', {
+				'action' : 'delete',
+				'id' : note_id
+			} ).done( function whenDone( data_text ){
+				note.remove();
 			} );
 		},
 		"show" : function( id, content ){
-			var note_wrapper = $.new( 'div', {
-				'class' : 'note-wrapper',
-				'id' : 'note' + id,
-				'data-id' : id,
-				'parent' : 'notes'
-			} );
-			var note = $.new( 'textarea', {
-				'class' : 'note',
-				'parent' : note_wrapper,
-				'onkeyup' : 'notes.update( this.parentNode );',
-				'value' : content
-			} );
-			var note_delete = $.new( 'button', {
-				'class' : 'note-delete',
-				'type' : 'button',
-				'onclick' : 'notes.delete( this.parentNode );',
-				'html' : "<i class='fa fa-ban'></i>",
-				'parent' : note_wrapper
-			} );
+			// Create note UI elements
+			var note_wrapper = $( "<div id='note" + id + "' class='note-wrapper' data-id='" + id + "'></div>" );
+			$( "#notes" ).append( note_wrapper );
+
+			var note = $( "<textarea class='note' onkeyup='notes.update( this.parentNode );'>" + content + "</textarea>" );
+			note_wrapper.append( note );
+
+			var note_delete = $( "<button class='note-delete' type='button' onclick='notes.delete( this.parentNode );'><i class='fa fa-ban'></i></button>" );
+			note_wrapper.append( note_delete );
 		},
 		"list" : function(){
-			getJSON( 'api.php?action=list', true, function( data ){
-				for( var i = 0; i < data.length; i++ ){
-					var note = data[i];
-					try{
-						notes.show( note.id, note.content );
-					}catch(e){
+			// Send list request to server, and show each note on screen when done
+			$.get( 'api.php', {
+				'action' : 'list'
+			} ).done( function whenDone( data_text ){
+				try{
+					// Convert response into JavaScript object
+					var data = JSON.parse( data_text );
 
+					// Object is array of notes, so loop through and show each one
+					for( var i = 0; i < data.length; i++ ){
+						var note = data[i];
+						notes.show( note.id, note.content );
 					}
+				}catch( e ){
+					console.log( 'Could not parse data for notes.list', data_text );
+					alert( 'Error parsing data. See console for details.' );
 				}
 			} );
 		},
 		"watch" : function(){
-			getJSON( 'api.php?action=list', true, function handleWatchResult( data ){
-				for( var i = 0; i < data.length; i++ ){
-					try{
-						var db_note = data[i];
-						var display_note = $.id( 'note' + db_note.id );
-						if( display_note == null ){
-							// Note is new, so show
-							notes.show( db_note.id, db_note.content );
-						}else{
-							var display_note_editor = display_note.getElementsByClassName( 'note' )[0];
-							display_note_editor.value = db_note.content;
-						}
-					}catch(e){
+			// Send list request to server, and sgiw each note on screen when done
+			$.get( 'api.php', {
+				'action' : 'list'
+			} ).done( function whenDone( data_text ){
+				try{
+					// Convert response into JavaScript object
+					var data = JSON.parse( data_text );
 
+					// Object is array of notes, so loop through and show each one
+					for( var i = 0; i < data.length; i++ ){
+						var note = data[i];
+						var display_note = $( '#note' + note.id );
+						if( display_note.length == 1 ){
+							// Note exists, so update content
+							var display_note_content = $( display_note.children( 'note' )[0] );
+							display_note_content.val( note.content );
+						}else{
+							// Note does not exist on page, so show it
+							notes.show( note.id, note.content );
+						}
 					}
+				}catch( e ){
+					console.log( 'Could not parse data for notes.list', data_text );
+					alert( 'Error parsing data. See console for details.' );
 				}
 			} );
 		}
