fb4fd769d4b6a0825ccd405f7173bf0ac181d6e7
Joseph Morrill <joseph.evan.morrill@gmail.com>
2016-03-11 00:46:34 -0500

    Note class

---
+++ StickyNotes/Domain/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickyNotes.Domain {
+	public class Note {
+		public Guid id { get; set; }
+		public string name { get; set; }
+		public string content { get; set; }
+		public int board { get; set; }
+		public DateTime created { get; set; }
+		public int status_code { get; set; }
+		public int creator { get; set; }
+		public int coord_x { get; set; }
+		public int coord_y { get; set; }
+		public int width { get; set; }
+		public int height { get; set; }
+	}
+}
